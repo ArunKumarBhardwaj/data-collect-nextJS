dc0546704e343688ba839da488b1ca68e4ff2552
ArunKumarBhardwaj <bhardwajarun38@gmail.com>
2024-07-08 20:10:29 +0530

    Revert "dummy commit"
    This reverts commit cfe47317cc84cfad28af1ce4d90c1ca4b4eb3184.

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>SKULLxDUDE IS GAY</p>
+    <main className="flex min-h-screen">
+      <p>HELLO WORLD</p>
     </main>
   );
 }
