be0e2cf1300824f31bcb4b65c20f47c59ca034f1
ArunKumarBhardwaj <bhardwajarun38@gmail.com>
2024-07-08 21:00:09 +0530

    register-user-form

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
-      <p>HELLO WORLD</p>
+    <main className="h-screen flex items-center justify-center p-24 bg-white">
+      <Card className="w-4/12">
+        <CardHeader>
+          <CardTitle>Register Form</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Label htmlFor="name">Name</Label>
+          <Input type="email" placeholder="Email" />
+        </CardContent>
+        <CardContent>
+          <Label htmlFor="name">Username</Label>
+          <Input type="email" placeholder="Email" />
+        </CardContent>
+        <CardContent>
+          <Label htmlFor="name">Password</Label>
+          <Input type="password" placeholder="Password" />
+        </CardContent>
+        <CardFooter className=" justify-center">
+          <Button className="w-full">Register</Button>
+        </CardFooter>
+      </Card>
     </main>
   );
 }
